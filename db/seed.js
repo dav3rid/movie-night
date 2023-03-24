80fad8a8357ad5d28f83ff47eae97c35e7965eb6
Dave Ridealgh <davecodingstuff@gmail.com>
2023-03-24 11:48:52 +0000

    update seed to include certificates, directors and movie references

--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,36 @@
-const { insertGenres, insertMovies } = require('./utils/insert-data');
+const {
+  insertGenres,
+  insertCertificates,
+  insertDirectors,
+  insertMovies
+} = require('./utils/insert-data');
 const { dropTables, createTables } = require('./utils/manage-tables');
 const { createRefObj, formatData } = require('./utils/utils');
 
-const seed = async ({ genres, movies }) => {
+const seed = async ({ genres, certificates, directors, movies }) => {
   await dropTables();
   await createTables();
 
-  const insertedGenres = await insertGenres(genres);
+  const [insertedGenres, insertedCertificates, insertedDirectors] =
+    await Promise.all([
+      insertGenres(genres),
+      insertCertificates(certificates),
+      insertDirectors(directors)
+    ]);
+
   const genreRef = createRefObj(insertedGenres, 'genre', 'genre_id');
-  const formattedMovies = formatData(genreRef, movies, 'genre_id', 'genre');
+  const certificateRef = createRefObj(
+    insertedCertificates,
+    'certificate',
+    'certificate_id'
+  );
+  const directorRef = createRefObj(insertedDirectors, 'name', 'director_id');
+
+  const formattedMovies = formatData(movies, [
+    [genreRef, 'genre_id', 'genre'],
+    [certificateRef, 'certificate_id', 'certificate'],
+    [directorRef, 'director_id', 'director']
+  ]);
   await insertMovies(formattedMovies);
 };
 
--- db/utils/insert-data.js
+++ db/utils/insert-data.js
@@ -19,16 +19,58 @@ exports.insertGenres = async (genres) => {
   return insertedGenres;
 };
 
+exports.insertCertificates = async (certificates) => {
+  const pgFormattedCertificates = certificates.map(({ certificate }) => {
+    return [certificate];
+  });
+  const insertCertificatesQueryString = format(
+    `
+    INSERT INTO certificates
+    (certificate)
+    VALUES
+    %L
+    RETURNING *;
+  `,
+    pgFormattedCertificates
+  );
+
+  const { rows: insertedCertificates } = await db.query(
+    insertCertificatesQueryString
+  );
+  return insertedCertificates;
+};
+
+exports.insertDirectors = async (directors) => {
+  const pgFormattedDirectors = directors.map(({ name }) => {
+    return [name];
+  });
+  const insertDirectorsQueryString = format(
+    `
+    INSERT INTO directors
+    (name)
+    VALUES
+    %L
+    RETURNING *;
+  `,
+    pgFormattedDirectors
+  );
+
+  const { rows: insertedDirectors } = await db.query(
+    insertDirectorsQueryString
+  );
+  return insertedDirectors;
+};
+
 exports.insertMovies = async (movies) => {
   const pgFormattedMovies = movies.map(
-    ({ title, genre_id, runtime, director, certificate }) => {
-      return [title, genre_id, runtime, director, certificate];
+    ({ title, genre_id, runtime, director_id, certificate_id }) => {
+      return [title, genre_id, runtime, director_id, certificate_id];
     }
   );
   const insertMoviesQueryString = format(
     `
     INSERT INTO movies
-    (title, genre_id, runtime, director, certificate)
+    (title, genre_id, runtime, director_id, certificate_id)
     VALUES
     %L;
   `,
